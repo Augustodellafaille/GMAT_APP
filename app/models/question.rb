30a40c166796fec45d21d59d8809233e636a9da8
Augustodellafaille <augusto.dellafaille@gmail.com>
2016-05-10 17:59:59 +0200

    trying to enum

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ActiveRecord::Base
+  # enum category: %w(Problem_Solving Data_Sufficiency Critical_Reasoning Reading_Comprehension Sentence_Correction)
+
+  def is_data_sufficiency?
+    category == "Data_Sufficiency"
+  end
 end
