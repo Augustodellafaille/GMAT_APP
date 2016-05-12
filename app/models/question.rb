210414999f4e21201fff626514ef99cdad8e949f
Augustodellafaille <augusto.dellafaille@gmail.com>
2016-05-12 10:55:53 +0200

    adding text file for reading text

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,9 @@ class Question < ActiveRecord::Base
   # enum category: %w(Problem_Solving Data_Sufficiency Critical_Reasoning Reading_Comprehension Sentence_Correction)
 
   def is_data_sufficiency?
-    category == "Data_Sufficiency"
+    category == "Data Sufficiency"
+  end
+  def is_reading_comprehension?
+    category == "Reading Comprehension"
   end
 end
