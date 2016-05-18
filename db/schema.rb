f0e1573d6e6ffc7b3bb35316eceb0e8b566d8efb
Augustodellafaille <augusto.dellafaille@gmail.com>
2016-05-18 15:19:41 +0200

    submit for a rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512090326) do
+ActiveRecord::Schema.define(version: 20160518084525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160512090326) do
     t.string   "sc_part_1"
     t.string   "sc_underline_part"
     t.string   "sc_part_2"
+    t.string   "math_topic"
+    t.string   "grammar_topic"
   end
 
 end
